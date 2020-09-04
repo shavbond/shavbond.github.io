dca52f3a251bcfc5fe026241e98696e3c9f9be8d
shavbond <58637774+shavbond@users.noreply.github.com>
2020-09-04 17:54:44 -0400

    added new project

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -30,7 +30,7 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Sample Projects</h1>
+            <h1>Projects</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
